95a974495c813062e44d6d70612d7f72273e183f
George <georgec.perez@gmail.com>
2015-09-25 01:16:58 -0700

    Updated hs_draw.rb to hearthstone.rb - made changes

---
+++ hearthstone.rb
@@ -0,0 +1,98 @@
+# Need array of cards in Deck
+
+=begin
+
+rockbiter weapon 2
+acidic swamp ooze 2
+bloodfen raptor 2
+flametongue totem 2
+murloc tidehunter 1
+hex 2
+razorfen hunter 2
+shattered sun cleric 2
+chillwind yeti 2
+gnomish inventor 2
+sen'jin shieldmasta 2
+bloodlust 1
+frostwolf warlod 2
+boulderfist ogre 2
+fire elemental 2
+stormwind champion 2
+
+=end
+
+class Hero
+	#@@
+	
+	# need hero stats
+	# hp = 30, armor = 0, attack = 0, cards?, hero_class?
+	
+	# Setting 'hero_power' need to make hashes {power name: stats}
+	def hero_power_setter(hero_class)
+		case hero_class.downcase!
+			when "druid"
+				hero_power = "shapeshift"
+			when "hunter"
+				hero_power = "steady shot"
+			when "mage"
+				hero_power = "fireblast"
+			when "paladin"
+				hero_power = "reinforce"
+			when "priest"
+				hero_power = "lesser_heal"
+			when "rogue"
+				hero_power = "dagger mastery"
+			when "shaman"
+				hero_power = "totemic call"				
+			when "warlock"
+				hero_power = "life tap"
+			when "warrior"
+				hero_power = "armor up!"
+			else
+				hero_power = "Class not found!"
+		end
+		return hero_power
+	end
+	
+	# Returning 'hero_power'
+	def hero_power_getter
+		hero_power = hero_power_setter(self.class.name)
+		return hero_power
+	end
+	
+end
+
+class Druid < Hero
+end
+
+class Hunter < Hero
+end
+
+class Mage < Hero
+end
+
+class Paladin < Hero
+end
+
+class Priest < Hero
+end
+
+class Rogue < Hero
+end
+
+class Shaman < Hero
+	#@
+end
+
+class Warlock < Hero
+end
+
+class Warrior < Hero
+end
+
+class Deck
+end
+
+eu_shaman = Shaman.new
+puts eu_shaman.hero_power_getter
+hand = [] # hand.draw(number) method?
